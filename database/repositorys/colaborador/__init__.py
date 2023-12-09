55968e84bcdd53cd4b44d05cc62587971aaa8321
feiticeiro-tec <silviohenriquecruzdasilva@gmail.com>
2023-12-09 23:37:18 +0000

    repo:colaborador:get_by_usuario_id

--- database/repositorys/colaborador/__init__.py
+++ database/repositorys/colaborador/__init__.py
@@ -15,8 +15,15 @@ class ColaboradorRepository:
         return cls(cls.get_by_id(id))
 
     @classmethod
-    def get_by_id(cls, id):
+    def get_by_id(cls, id) -> Colaborador:
         colaborador = Colaborador.query.filter_by(id=id).first()
+        if not colaborador:
+            raise cls.NotFoundColaboradorException()
+        return colaborador
+
+    @classmethod
+    def get_by_usuario_id(cls, usuario_id) -> Colaborador:
+        colaborador = Colaborador.query.filter_by(usuario_id=usuario_id).first()
         if not colaborador:
             raise cls.NotFoundColaboradorException()
         return colaborador
--- tests/test_repositorys/test_colaborador_repository.py
+++ tests/test_repositorys/test_colaborador_repository.py
@@ -97,3 +97,44 @@ def test_use_by_id_not_found(app):
     with app.app_context():
         with pytest.raises(ColaboradorRepository.NotFoundColaboradorException):
             ColaboradorRepository.use_by_id("123")
+
+
+def test_get_by_usuario_id(app):
+    with app.app_context():
+        usuario = UsuarioRepository.new(
+            nome="teste",
+            email="email",
+            senha="senha",
+        )
+        usuario.add()
+        db.session.flush()
+        usuario_id = usuario.usuario.id
+        cinema = CinemaRepository.new(
+            nome="cinema",
+            descricao="descricao",
+            endereco=CinemaRepository.Endereco(
+                cep="cep",
+                uf="uf",
+                cidade="cidade",
+                bairro="bairro",
+                rua="rua",
+                numero=1,
+                complemento="complemento",
+                referencia="referencia",
+            ),
+        )
+        cinema.add()
+        db.session.flush()
+        cinema_id = cinema.cinema.id
+        colaborador = Colaborador(
+            usuario_id=usuario_id,
+            cinema_id=cinema_id,
+        )
+        db.session.add(colaborador)
+        db.session.commit()
+
+    with app.app_context():
+        colaborador = ColaboradorRepository.get_by_usuario_id(usuario_id)
+        assert colaborador.usuario_id == usuario_id
+        assert colaborador.cinema_id == cinema_id
+
