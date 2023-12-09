c92f1185fbf732af2d58b168ddbb7525d89bdafb
feiticeiro-tec <silviohenriquecruzdasilva@gmail.com>
2023-12-09 21:40:51 +0000

    repo:cinema:set_endereco

--- database/repositorys/cinema/__init__.py
+++ database/repositorys/cinema/__init__.py
@@ -1,18 +1,30 @@
 from ...models import Cinema
 from .exceptions import NotFoundCinemaException
+from .intefaces import Endereco
 
-class CinemaRepository():
+
+class CinemaRepository:
     NotFoundCinemaException = NotFoundCinemaException
+    Endereco = Endereco
 
-    def __init__(self, cinema:Cinema= None):
+    def __init__(self, cinema: Cinema = None):
         if not cinema:
             cinema = Cinema()
         self.cinema = cinema
-    
+
     @classmethod
     def use_by_id(cls, id):
         cinema = Cinema.query.filter_by(id=id).first()
         if not cinema:
             raise cls.NotFoundCinemaException()
         return cls(cinema)
-    
+
+    def set_endereco(self, endereco: "Endereco"):
+        self.cinema.cep = endereco.cep
+        self.cinema.uf = endereco.uf
+        self.cinema.cidade = endereco.cidade
+        self.cinema.bairro = endereco.bairro
+        self.cinema.rua = endereco.rua
+        self.cinema.numero = endereco.numero
+        self.cinema.complemento = endereco.complemento
+        self.cinema.referencia = endereco.referencia
--- tests/test_repositorys/test_cinema_repository.py
+++ tests/test_repositorys/test_cinema_repository.py
@@ -25,3 +25,36 @@ def test_use_by_id(app):
         assert repo.cinema.id == cinema_id
         assert isinstance(repo, CinemaRepository)
         assert isinstance(repo.cinema, Cinema)
+
+
+def test_set_endereco(app):
+    with app.app_context():
+        cinema = Cinema(
+            nome="Cinema 1",
+            descricao="Cinema 1",
+        )
+        enderco = CinemaRepository.Endereco(
+            cep="00000000",
+            uf="Estado 1",
+            cidade="Cidade 1",
+            bairro="Bairro 1",
+            rua="Rua 1",
+            numero=1,
+            complemento="Complemento 1",
+        )
+        CinemaRepository(cinema).set_endereco(enderco)
+        app.db.session.add(cinema)
+        app.db.session.commit()
+        cinema_id = cinema.id
+
+    with app.app_context():
+        cinema = Cinema.query.filter(Cinema.id == cinema_id).first()
+        assert cinema
+        assert cinema.cep == enderco.cep
+        assert cinema.uf == enderco.uf
+        assert cinema.cidade == enderco.cidade
+        assert cinema.bairro == enderco.bairro
+        assert cinema.rua == enderco.rua
+        assert cinema.numero == enderco.numero
+        assert cinema.complemento == enderco.complemento
+        assert cinema.referencia == enderco.referencia
