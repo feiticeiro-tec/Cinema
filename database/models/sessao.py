86a72e353a7a6618adb262ed0e5f057244888599
feiticeiro-tec <silviohenriquecruzdasilva@gmail.com>
2023-12-09 20:30:36 +0000

    add model sessao

---
+++ database/models/sessao.py
@@ -0,0 +1,18 @@
+from .. import db
+from database.adapters.type import UUID
+from sqlalchemy.orm import relationship
+
+
+class Sessao(db.Model):
+    __tablename__ = "Sessao"
+    id = db.Column(UUID, primary_key=True)
+    dt_inicio = db.Column(db.Datetime, nullable=False)
+    dt_fim = db.Column(db.Datetime, nullable=False)
+
+    sala_id = db.Column(UUID, db.ForeignKey("Sala.id"), nullable=False)
+    sala = relationship("Sala", backref="sessoes")
+
+    filme_id = db.Column(UUID, db.ForeignKey("Filme.id"), nullable=False)
+    filme = relationship("Filme", backref="sessoes")
+
+    dt_criado = db.Column(db.Datetime, nullable=False, default=db.func.now())
