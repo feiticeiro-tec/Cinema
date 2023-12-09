265d63a30bdcaa26c13d327063a1b103acb53fcb
feiticeiro-tec <silviohenriquecruzdasilva@gmail.com>
2023-12-09 20:37:22 +0000

    add model ticket and fix name acento to assento

--- database/models/assento.py
+++ database/models/assento.py
@@ -2,17 +2,17 @@ from .. import db
 from database.adapters.type import UUID
 from sqlalchemy import UniqueConstraint
 
-class Acento(db.Model):
-    __tablename__ = "Acento"
+class Assento(db.Model):
+    __tablename__ = "Assento"
     __table_args__ = (
-        UniqueConstraint("acento", "sala_id", name="uq_acento_sala"),
+        UniqueConstraint("assento", "sala_id", name="uq_assento_sala"),
     )
     
     id = db.Column(UUID, primary_key=True)
-    acento = db.Column(db.String(50), nullable=False)
+    assento = db.Column(db.String(50), nullable=False)
     is_ativo = db.Column(db.Boolean, nullable=False, default=True)
 
     sala_id = db.Column(UUID, db.ForeignKey("Sala.id"), nullable=False)
-    sala = db.relationship("Sala", backref="acentos")
+    sala = db.relationship("Sala", backref="assentos")
 
     
---
+++ database/models/ticket.py
@@ -0,0 +1,17 @@
+from .. import db
+from database.adapters.type import UUID
+from database.adapters.functions import datetime_local
+from sqlalchemy.orm import relationship
+
+class Ticket(db.Model):
+    id = db.Column(UUID, primary_key=True)
+    sessao_id = db.Column(UUID, db.ForeignKey("Sessao.id"), nullable=False)
+    sessao = relationship("Sessao", backref="tickets")
+    usuario_id = db.Column(UUID, db.ForeignKey("Usuario.id"), nullable=False)
+    usuario = relationship("Usuario", backref="tickets")
+    assento_id = db.Column(UUID, db.ForeignKey("Assento.id"), nullable=False)
+    assento = relationship("Assento", backref="tickets")
+    is_cancelado = db.Column(db.Boolean, nullable=False, default=False)
+    is_usado = db.Column(db.Boolean, nullable=False, default=False)
+    dt_criado = db.Column(db.Datetime, nullable=False, default=datetime_local)
+
