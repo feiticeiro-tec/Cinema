e19d227959dac20744ae0d67c833e2ee444e3b2c
feiticeiro-tec <silviohenriquecruzdasilva@gmail.com>
2023-12-10 02:00:23 +0000

    removendo subtests

--- tests/test_repositorys/test_usuario_repository.py
+++ tests/test_repositorys/test_usuario_repository.py
@@ -51,38 +51,6 @@ def test_use_by_id_not_found(app):
         with pytest.raises(Exception):
             UsuarioRepository.use_by_id("123")
 
-
-def test_set_credencial(app):
-    with app.app_context():
-        usuario = Usuario(nome="teste")
-        repo = UsuarioRepository(usuario)
-        repo.set_credencial("email", "senha")
-        repo.add()
-        repo.commit()
-        usuario_id = repo.usuario.id
-
-    with app.app_context():
-        usuario = Usuario.query.filter(Usuario.id == usuario_id).first()
-        assert usuario
-        assert usuario.email == "email"
-        assert usuario.senha == "senha"
-
-
-def test_set_nome(app):
-    with app.app_context():
-        usuario = Usuario(email="email", senha="senha")
-        repo = UsuarioRepository(usuario)
-        repo.set_nome("teste2")
-        repo.add()
-        repo.commit()
-        usuario_id = repo.usuario.id
-
-    with app.app_context():
-        usuario = Usuario.query.filter(Usuario.id == usuario_id).first()
-        assert usuario
-        assert usuario.nome == "teste2"
-
-
 def test_set_is_ativo(app):
     with app.app_context():
         usuario = Usuario(email="email", senha="senha", nome="teste")
