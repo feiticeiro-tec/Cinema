4a8a0d44a46d5e006e2ab1d2022eb0f306f46f85
feiticeiro-tec <silviohenriquecruzdasilva@gmail.com>
2023-12-11 22:16:10 +0000

    add cors

--- api/__init__.py
+++ api/__init__.py
@@ -1,12 +1,15 @@
 from flask_restx import Api
 from importlib import import_module
 from flask_jwt_extended import JWTManager
+from flask_cors import CORS
 
 api = Api()
 jm = JWTManager()
+cors = CORS()
 
 
 def init_app(app):
     api.init_app(app)
     jm.init_app(app)
+    cors.init_app(app)
     import_module(".namespaces", package=__name__)
